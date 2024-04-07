4295be698887097a4eb382254a65865c9204c1f8
Oksana Miazina <oksana.myazina@gmail.com>
2024-04-07 17:37:29 +0300

    feat: add services for CinemaHall and MovieSession

---
+++ src/main/java/mate/academy/service/impl/CinemaHallServiceImpl.java
@@ -0,0 +1,31 @@
+package mate.academy.service.impl;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import mate.academy.dao.CinemaHallDao;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.CinemaHall;
+import mate.academy.service.CinemaHallService;
+
+@Service
+public class CinemaHallServiceImpl implements CinemaHallService {
+    @Inject
+    private CinemaHallDao cinemaHallDao;
+
+    @Override
+    public CinemaHall add(CinemaHall cinemaHall) {
+        return cinemaHallDao.add(cinemaHall);
+    }
+
+    @Override
+    public CinemaHall get(Long id) {
+        return cinemaHallDao.get(id).orElseThrow(
+                () -> new NoSuchElementException("Can't get a cinema hall with id " + id));
+    }
+
+    @Override
+    public List<CinemaHall> getAll() {
+        return cinemaHallDao.getAll();
+    }
+}
